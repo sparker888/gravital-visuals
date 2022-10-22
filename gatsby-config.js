d64f14c7865ce16030d2e08733293b4d85860008
Stephen Parker <sparker888@gmail.com>
2022-10-22 14:16:03 -0400

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    links: {
+      facebook: `https://www.facebook.com/gogravital/`,
+      twitter: `https://twitter.com/gogravital/`,
+    },
+    locale: `en`,
+    title: `Gravital Visuals | Drones, Photography & Videography`,
+    titleTemplate: `Gravital Visuals | Photography & Videography`,
+    description: `Drones, Photography and Videography.`,
+    siteUrl: `https://www.gravitalvisuals.com`, // No trailing slash allowed!
+    twitterUsername: `@gogravital`,
+    publicURL: `/rocket-social-card.jpg`, // This will be the default 'social' image for social media shares (in the static folder)
+    organization: `Gravital Visuals`,
+    author: `sparker888`,
   },
   plugins: [
     `gatsby-plugin-image`,
@@ -13,21 +22,44 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
+      __key: `images`,
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+      __key: `pages`,
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `gravital-visuals`,
+        short_name: `gravital`,
         start_url: `/`,
         background_color: `#663399`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        // theme_color: `#663399` - change this to the company brand color,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-postcss`,
+      options: {
+        postCssPlugins: [require("tailwindcss"), require("autoprefixer")],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: false,
+        develop: false,
+        tailwind: true,
       },
     },
   ],
